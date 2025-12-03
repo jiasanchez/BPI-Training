061b45e541bd2ffd0b5ae0fef91d27653cafc375
John Isiah  A. Sanchez <jiasanchez@headoffice.corp.bpi.com.ph>
2025-12-03 09:49:09 +0800

    Activity 1

---
+++ BasicActivity1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Training;
+
+import java.util.Scanner;
+/**
+ * 
+ */
+public class BasicActivity1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("What is your name? ");
+		String input = scanner.next();
+		
+		System.out.println("Hello, " + input + "!");
+	}
+
+}
