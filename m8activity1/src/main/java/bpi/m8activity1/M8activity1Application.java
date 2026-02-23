a538bb910427e859ea4393b5d264273b5cd1eab1
John Isiah  A. Sanchez <jiasanchez@headoffice.corp.bpi.com.ph>
2026-02-23 23:21:41 +0800

    M8 Act1

--- m8activity1/src/main/java/bpi/m8activity1/M8activity1Application.java
+++ m8activity1/src/main/java/bpi/m8activity1/M8activity1Application.java
@@ -1,24 +1,13 @@
 package bpi.m8activity1;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class M8activity1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(M8activity1Application.class, args);
-	}
-		//Activity 1
-		@Bean
-		CommandLineRunner run(BookService bookService) {
-			return args -> {
-				bookService.createBook("Test Demo");
-			};
-		
 	}
 
 }
